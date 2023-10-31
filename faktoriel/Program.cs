f0e0e920f5b3ce47b1a7f36b718d022d7610660f
arminlry <arminlry@DESKTOP-H8CJ8C9>
2023-11-01 01:51:32 +0330

    Add project files.

---
+++ faktoriel/Program.cs
@@ -0,0 +1,21 @@
+﻿//armin liryaee
+// tamrin 5
+//factoriel
+char p = ' ';
+
+
+do
+{
+    Console.WriteLine(" enter N number:");
+    decimal N = Convert.ToDecimal(Console.ReadLine());
+    decimal a = N;
+    for (decimal i = 1; i < N; i++)
+    {
+        a = a * i;
+    }
+    Console.WriteLine(p);
+    Console.WriteLine(a);
+    Console.WriteLine(p);
+    Console.WriteLine("do you want to continue?");
+
+} while (Console.ReadLine() == "y");
